e32bd41a93de4769bb33349f69b7e4fea5f4c5b5
mabbott4 <145649889+mabbott4@users.noreply.github.com>
2023-09-27 07:45:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+def main():
+    intro() 
+    
+    try:
+        miles = int(input("Please enter number of miles: ")) # get number of miles
+        miles_to_kilometers(miles)  
+    except ValueError:
+        print("An error occurred, please enter a number only.")
+        print()
+        main()
+
+def intro():
+    print("This program converts measurements in miles to kilometers.")
+    print("For your reference, the conversion is 1 mile = 1.609 kilometers.")
+    print()
+
+def miles_to_kilometers(miles):  # convert miles to kilometers
+    kilometers = miles * 1.60934
+  # display total miles and kilometers 
+    print(miles, "miles converts to", kilometers, "kilometers.")
+
+main() #call the main function
